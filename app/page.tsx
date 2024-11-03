25351b45efff6d0fa868220138fa00c0da862573
Akangkha <akangkhasarkar@gmail.com>
2024-11-03 12:41:07 +0530

    Assets added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <>HomePage</>;
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+    <div>page</div>
+  )
 }
+
+export default page
